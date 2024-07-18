e695f20883227896f191eb8d3108fec38ce8ef0a
Kirin Patel <kirinpatel@gmail.com>
2024-07-18 18:39:20 -0400

    🐶 start working on /dogs CRUD operations

--- backend/dog_stuff/dogs.py
+++ backend/dog_stuff/dogs.py
@@ -1,7 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from json import loads
 
 bp = Blueprint('dogs', __name__, url_prefix='/dogs')
 
-@bp.route("/bork", methods=["GET"])
-def file_upload_handler():
-	return "bark"
+dogs = []
+
+@bp.route("/", methods=["GET", "POST"])
+def dogs_handler():
+	if request.method == "GET":
+		return dogs
+	
+	request_data = loads(request.data)
+
+	if "name" not in request_data:
+		return "Name not provided"
+	
+	name = request_data["name"]
+
+	dogs.append({ "name": name })
+	return f"Welcome {name} to the pack!"
+
+@bp.route("/<name>", methods=["GET", "PATCH", "DELETE"])
+def dog_handler(name):
+	dog = next(filter(lambda dog: dog["name"] == name, dogs), None)
+
+	if dog is None:
+		return "No dog with that name found!"
+	
+	if request.method == "GET":
+		return dog
+	
+	return "not yet implemented :("
